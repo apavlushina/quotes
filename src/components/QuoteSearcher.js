c5d507a4309639432120240e33fa5cc39d60de6f
apavlushina <ssemina7@gmail.com>
2019-10-26 14:34:09 +0200

    step 6

--- src/components/QuoteSearcher.js
+++ src/components/QuoteSearcher.js
@@ -4,14 +4,14 @@ import Quote from "./Quote";
 export default class QuoteSearcher extends React.Component {
   state = {
     quotes: [],
-    fetching: true
+    fetching: true,
+    search: ""
   };
 
-  componentDidMount() {
-    fetch("https://quote-garden.herokuapp.com/quotes/search/tree")
+  componentDidMount(keyword = "tree") {
+    fetch(`https://quote-garden.herokuapp.com/quotes/search/${keyword}`)
       .then(res => res.json())
       .then(response => {
-        console.log("response", response);
         this.setState({
           quotes: response.results,
           fetching: false
@@ -20,9 +20,36 @@ export default class QuoteSearcher extends React.Component {
       .catch(console.error);
   }
 
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.componentDidMount(this.state.search);
+    this.setState({ [event.target.name]: "" });
+  };
+
   render() {
     return (
       <div>
+        <p>Do you want smthing special?</p>
+
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="search"
+            placeholder="type your request here"
+            onChange={this.handleChange}
+            value={this.state.search}
+          />
+          <input
+            type="submit"
+            value="Search the quote for me!"
+            onClick={this.handleSearch}
+          />
+        </form>
+
         <ul>
           {this.state.fetching && "Loading..."}
           {!this.state.fetching &&
